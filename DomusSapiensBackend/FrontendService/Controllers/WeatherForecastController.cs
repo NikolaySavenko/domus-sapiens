7d5d7364629378985af3130727153b9ea775c44b
Nikolay Savenko <savenkons@yandex.ru>
2021-10-09 22:56:55 +0300

    refactor: fix formatting

--- DomusSapiensBackend/FrontendService/Controllers/WeatherForecastController.cs
+++ DomusSapiensBackend/FrontendService/Controllers/WeatherForecastController.cs
@@ -6,10 +6,10 @@ namespace FrontendService.Controllers
 	[Route("[controller]")]
 	public class WeatherForecastController : ControllerBase
 	{
-		private static readonly string[] Summaries = new[]
+		private static readonly string[] Summaries = new[] 
 		{
-		"Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
-	};
+			"Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+		};
 
 		private readonly ILogger<WeatherForecastController> _logger;
 
