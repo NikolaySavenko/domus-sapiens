2509171f20820c366e106edc74a57b9bc565fd7e
Nikolay Savenko <savenkons@yandex.ru>
2021-10-14 22:43:10 +0300

    feat: simple user and device data classes

---
+++ DomusSapiensBackend/FrontendService/Model/Device.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+namespace FrontendService.Model
+{
+	public class Device
+	{
+		public int DeviceId { get; set; }
+		public int UserId { get; set; }
+	}
+}
---
+++ DomusSapiensBackend/FrontendService/Model/User.cs
@@ -0,0 +1,8 @@
+﻿namespace FrontendService.Model
+{
+	public class User
+	{
+		public int UserId { get; set; }
+		public List<Device> Devices { get; set; }
+	}
+}
