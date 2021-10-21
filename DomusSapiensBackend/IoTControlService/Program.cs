0fdbb1192acd9c62bbcabca1dfa777e6404c891f
Nikolay Savenko <savenkons@yandex.ru>
2021-10-21 17:08:55 +0300

    feat: add high level commands consumer

--- DomusSapiensBackend/IoTControlService/Program.cs
+++ DomusSapiensBackend/IoTControlService/Program.cs
@@ -5,6 +5,7 @@ var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllers();
 builder.Services.AddHostedService<IoTQueueConsumer>();
+builder.Services.AddHostedService<HighLevelCommendsConsumer>();
 builder.Services.AddSingleton<IProcessData, ProcessData>();
 
 var app = builder.Build();
