3e59ee0afdc8875224287da23ffc9f236467cb86
Nikolay Savenko <savenkons@yandex.ru>
2021-10-21 12:17:38 +0300

    feat: simple process data

---
+++ DomusSapiensBackend/IoTControlService/ServiceBusMessaging/IProcessData.cs
@@ -0,0 +1,24 @@
+﻿namespace IoTControlService.ServiceBusMessaging
+{
+	public interface IProcessData
+	{
+		Task Process(MyPayload myPayload);
+	}
+	public class MyPayload
+	{
+
+	}
+    public class ProcessData : IProcessData
+    {
+        private IConfiguration _configuration;
+
+        public ProcessData(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+        public async Task Process(MyPayload myPayload)
+        {
+            
+        }
+    }
+}
