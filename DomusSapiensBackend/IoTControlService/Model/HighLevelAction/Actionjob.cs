7f0171ffc8498b6bba0e7b8818d90de0a5008d37
Nikolay Savenko <savenkons@yandex.ru>
2021-10-21 22:56:46 +0300

    feat(model): high level command model

---
+++ DomusSapiensBackend/IoTControlService/Model/HighLevelAction/Actionjob.cs
@@ -0,0 +1,12 @@
+﻿using IoTControlService.Model.DeviceMethods;
+
+namespace IoTControlService.Model.HighLevelAction
+{
+	public class Actionjob
+	{
+		public string Name { get; init; }
+
+		public List<IDeviceMethod> Methods { get; init; }
+		// TODO Serialization
+	}
+}
---
+++ DomusSapiensBackend/IoTControlService/Model/HighLevelAction/HighLevelAction.cs
@@ -0,0 +1,9 @@
+﻿namespace IoTControlService.Model.HighLevelAction
+{
+	public class HighLevelAction
+	{
+		public string Action { get; set; }
+		public Guid Device { get; set; }
+		public List<(string, string)> Params { get; set; }
+	}
+}
