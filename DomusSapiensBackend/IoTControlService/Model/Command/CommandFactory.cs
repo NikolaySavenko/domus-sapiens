7e470f8f2bc6b013752227b745549062a3bf6767
Nikolay Savenko <savenkons@yandex.ru>
2021-10-21 22:57:07 +0300

    feat(model): command method model

---
+++ DomusSapiensBackend/IoTControlService/Model/Command/CommandFactory.cs
@@ -0,0 +1,25 @@
+﻿using IoTControlService.Model.HighLevelAction;
+using IoTControlService.Model.DeviceMethods;
+
+namespace IoTControlService.Model.Command
+{
+	// Factory Pattern
+	public class CommandFactory
+	{
+		private Command _executable;
+
+		public CommandFactory(string commandName, Guid deviceId)
+		{
+			_executable = new Command { DeviceId = deviceId };
+		}
+
+		public IExecutable BuildCommand()
+		{
+			//TODO get Actionjob from redis
+			Actionjob job = new Actionjob { Name = "JobFromRedis", Methods = new List<IDeviceMethod>() };
+
+			_executable._methods = job.Methods;
+			return _executable;
+		}
+	}
+}
